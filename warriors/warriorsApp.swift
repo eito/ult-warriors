521be9f5edaea065412683f2b1551fea176ce9e2
Eric Ito <eric.ito@gmail.com>
2022-03-11 22:45:52 -0800

    Initial Commit

---
+++ warriors/warriorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  warriorsApp.swift
+//  warriors
+//
+//  Created by Eric Ito on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct warriorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
